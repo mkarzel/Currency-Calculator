871a2d407225b75e180c360f416ec6c6b3f784e2
mkarzel <karzelmarcin@gmail.com>
2020-01-18 19:57:43 +0100

    zadanie 2 - poprawki

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
+const display = document.querySelector("#display");
 let expression = ''
 let currentNumber = ''
 const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 const operators = ['+', '-', '*', '/', '^', 'r']
 let lastOperatorIndex = -1
-const currencies = ['USD', 'EUR', 'GBP', 'CHF', 'JPY']
+let currencies = []
 let rates = []
 
 const insert = (entry) => {
+    if (validateInsert(entry)){
+        currentNumber = currentNumber + entry
+        expression = expression + entry
+    }
+}
+
+const validateInsert = (entry) => {
     // indexing operators in expression
     for (let i = 0; i < operators.length; i++) {
         if (expression.lastIndexOf(operators[i]) > lastOperatorIndex) {
@@ -102,13 +110,11 @@ const insert = (entry) => {
     if (currencies.includes(expression.slice(-3)) && !(entry.includes('+') || entry.includes('-') || entry.includes('*') || entry.includes('/') || entry.includes(')'))) {
         return
     }
-    // no currency if last operator was r or ^
-    if (currencies.includes(entry) && lastOperatorIndex !== -1 && (lastOperatorIndex === expression.lastIndexOf('r') || lastOperatorIndex === expression.lastIndexOf('^'))) {
+    // no currency if last operator was r, ^, /
+    if (currencies.includes(entry) && lastOperatorIndex !== -1 && (lastOperatorIndex === expression.lastIndexOf('r') || lastOperatorIndex === expression.lastIndexOf('^') || lastOperatorIndex === expression.lastIndexOf('/'))) {
         return
     }
-
-    currentNumber = currentNumber + entry
-    expression = expression + entry
+    return true
 }
 
 const clearAll = () => {
@@ -137,7 +143,7 @@ const equal = () => {
     if ((expression.match(/\(/g) || []).length > (expression.match(/\)/g) || []).length) {
         return
     }
-    //replace currency with * rate
+    // replace currency with * rate
     for (let i = 0; i < currencies.length; i++) {
         expression = expression.split(currencies[i]).join('*' + rates[i]);
     }
@@ -152,9 +158,12 @@ const equal = () => {
     expression = expression.replace(/(\d+\.?\d*)\s*r\s*(-*\d+\.?\d*)/g, 'Math.pow($1, 1/$2)')
 
     // calculate expression unless it displays dividing by 0 error
-    if (expression !== "nie dzielimy przez 0") {
-        expression = eval(expression).toString()
+    if (expression !== ""){
+        if (expression !== "nie dzielimy przez 0") {
+            expression = eval(expression).toString()
+        }
     }
+
     // dividing by 0 error
     if (expression === "Infinity" || expression === "-Infinity" || expression === "nie dzielimy przez 0") {
         expression = "nie dzielimy przez 0"
@@ -165,32 +174,34 @@ const equal = () => {
         expression = ''
     }
     // round result if too many numbers after .
-    if(expression.length > 8 && expression.includes('.')){
+    if (expression.length > 8 && expression.includes('.')) {
         expression = eval(expression).toFixed(4).toString()
     }
     currentNumber = expression
 }
 
 document.addEventListener('click', () => {
-    document.querySelector("#display").value = expression
+    display.value = expression
     // display 0 after clearing
     if (expression === '') {
-        document.querySelector("#display").value = '0'
+        display.value = '0'
     }
 })
 
 const getRates = async () => {
-    for (let i = 0; i < currencies.length; i++) {
-        await fetch("http://api.nbp.pl/api/exchangerates/rates/A/" + currencies[i] + "/")
-            .then(async (response) => {
-                return await response.json();
-            })
-            .then((currenciesJson) => {
-                rates.push(currenciesJson.rates[0].mid)
-            }).catch((error) => {
-                console.error('Currencies are not loaded, please refresh', error);
-            });
-        document.querySelector(".dropdown-menu").innerHTML += '<button class="dropdown-item" onclick="insert(\'' + currencies[i] + '\')">' + currencies[i] + '</button>'
+    try {
+        response = await fetch("http://api.nbp.pl/api/exchangerates/tables/a")
+        currenciesData = await response.json()
+
+        for (let i = 0; i < currenciesData[0].rates.length; i++) {
+            currencies.push(currenciesData[0].rates[i].code)
+            rates.push(currenciesData[0].rates[i].mid)
+            document.querySelector(".dropdown-menu").innerHTML += '<button class="dropdown-item" onclick="insert(\'' + currencies[i] + '\')">' + currencies[i] + '</button>'
+        }
+    }
+    catch (error) {
+        console.error('Currencies are not loaded', error);
     }
 }
 
+window.onload = getRates()
